fe2cc1d928f1bd9822fb5e2b30cc71bacaab34bb
Albert Diones <adiones@addteam.org>
2024-11-11 22:43:27 +0800

    test interval play

--- index.ts
+++ index.ts
@@ -1,20 +1,46 @@
+import Repeater from "add_repeater";
 
+interface player {
+    play: (
+        fileName: string, 
+        errorCallback: (err: string) => void
+    ) => void;
+}
 
 export class HeartBeep {
 
+    
+    interval: number;
+    strategies: AudioStrategy[];
+    defaultAudio: string;
+    player: player;
+
     constructor(
         options: {
-            interval: number, 
-            strategies: AudioStrategy[], 
-            defaultAudio: string
-        }
+            interval?: number, 
+            strategies?: AudioStrategy[], 
+            defaultAudio?: string,
+            player?: player
+        } = {}
     ) {
+        this.interval = options.interval ?? 1000;
+        this.strategies = options.strategies ?? [];
+        this.defaultAudio = options.defaultAudio ?? 'xxxxxx';
+        this.player = options.player ?? require('play-sound')();
+    }
 
+    start() {
+        setInterval(
+            () => {
+                this.player.play(this.defaultAudio, () => {})
+            },
+            this.interval
+        );
     }
 
 }
 
-interface AudioStrategy {
+export interface AudioStrategy {
 
     match(): boolean;
 
--- tests/first.test.ts
+++ tests/first.test.ts
@@ -9,3 +9,28 @@ test(
         expect(beep).toBeInstanceOf(HeartBeep);
     }
 )
+
+let playedSound: string[] = [];
+
+const mockPlayer = {
+    play: (
+        file: string,
+        errorHandler: () => void,
+    ) => {
+        playedSound.push(file);
+        // require('play-sound')().play(file);
+    }
+}
+
+test(
+    'first test',
+    async () => {
+        const beep = new HeartBeep({ 
+            interval: 200,
+            player: mockPlayer
+        });
+        beep.start();
+        await Bun.sleep(500);
+        expect(playedSound.length).toBe(2);
+    }
+)
